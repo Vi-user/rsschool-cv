e0b3913fd224c9516759c5d3b027c82c809c23da
Vi-user <vi.volkova.2712@gmail.com>
2022-10-15 22:07:05 +0300

    feat: burger menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+//menuScroll
+const menuItems = document.querySelectorAll('.navigation__link[data-goto]')
+console.log(menuItems)
+const scrollToBlock = (e) => {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+        if(burger.classList.contains('menu-open')) {
+            document.body.classList.toggle('scroll-lock')
+            burger.classList.toggle('menu-open')
+            document.querySelector('.header__navigation').classList.toggle('header__navigation_active')
+        }
+
+        window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+        });
+        e.preventDefault();
+    }
+}
+
+if (menuItems.length > 0) {
+    menuItems.forEach(el => {
+        el.addEventListener('click', scrollToBlock)
+    })
+}
+
 //scrollBTN
 const scrollBtn = document.querySelector('.scroll-btn');
 const docHeight = document.documentElement.clientHeight;
@@ -36,7 +65,9 @@ themeIcon.addEventListener('click', changeTheme)
 const burger = document.querySelector('.burger');
 
 const showMenu = () => {
-    console.log('menu')
+    document.body.classList.toggle('scroll-lock')
+    burger.classList.toggle('menu-open')
+    document.querySelector('.header__navigation').classList.toggle('header__navigation_active')
 }
 
-burger.addEventListener('click', showMenu)
+burger.addEventListener('click', showMenu);
